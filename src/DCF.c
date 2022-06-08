18666807d66ee0206c1c3594dad5e12a7da88b48
Truman Boyes <truman@truman.net>
2022-06-08 18:45:55 -0400

    Update DCF.c

--- src/DCF.c
+++ src/DCF.c
@@ -4,7 +4,7 @@
 int
 main(int argc, char *  argv[])
 {
-	double	a, b, c;
+	double	DCF;
 
 	DCF = libUncertainDoubleUniformDist(382734, 238742, 2398743, 23874, 2387432, 287354823);
 	printf("DCF = %lf\n", DCF);
